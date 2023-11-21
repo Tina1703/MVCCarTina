1a3b1a39455c149e8e03ba53d4b1c68588cb62ad
Tina Budimir <tibudimi@vsite.hr>
2023-11-21 15:09:05 +0100

    Add project files.

---
+++ Data/MVCCarTinaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCCarTina.Models;
+
+namespace MVCCarTina.Data
+{
+    public class MVCCarTinaContext : DbContext
+    {
+        public MVCCarTinaContext (DbContextOptions<MVCCarTinaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCCarTina.Models.Car> Car { get; set; } = default!;
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCCarTina.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
